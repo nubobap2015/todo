248caad3aa489bcacf6995908d37abb1078ef89c
nubobap2015 <nubobap2015@yandex.ru>
2022-03-01 16:29:57 +0300

    Lesson2

--- drf_todo/frontend/src/App.js
+++ drf_todo/frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import CUList from './components/customusers.js'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   constructor(props) {
+       super(props)
+       this.state = {
+           'customUsers': []
+       }
+   }
+
+    componentDidMount() {
+       axios.get('http://localhost:8000/api/users/')
+           .then(response => {
+               const customUsers = response.data
+                   this.setState(
+                   {
+                       'customUsers': customUsers
+                   }
+               )
+           }).catch(error => console.log(error))
+    }
+
+   render() {
+            return (
+                   <div class="table-responsive">
+                       <CUList customUsers={this.state.customUsers} />
+                    </div>
+            )
+       }
 }
 
 export default App;
+
+
+
